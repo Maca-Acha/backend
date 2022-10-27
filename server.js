af71058836f30a962e7ff7813814ef1e2aaa9fc8
Maca-Acha <achamariamacarena@gmail.com>
2022-10-27 17:34:45 -0300

    servidor

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 
-app.listen(4000, ()=>{console.log("Sever is listening on port 4000")})
+app.use(express.static("client/build"))
+app.get("*", (req,res)=>{
+    res.status(200).sendFile(path.join(__dirname + "/client/build/index.html" ))
+})
+
+app.listen(4000, ()=>{console.log("Server is listening on port 4000")})
+
